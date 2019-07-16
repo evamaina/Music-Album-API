120edf7b9ef82a5c73013096b6a8bfdc78478a85
evamaina <evajohnson714@gmail.com>
2019-07-16 14:11:02 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList.js';
+
+const App = () => (
+    <View style={{ flex: 1 }}>
+        <Header headerText={'Albums!'} />
+        <AlbumList />
+    </View>
+      
+    );
+
+AppRegistry.registerComponent('albums', () => App);
